f48e2234d3f3ae240f3c483be4029457b1b44db7
yujin_sakuma <yujin12623@gmail.com>
2020-04-13 23:42:19 +0900

    Arrange Responsible

--- map.js
+++ map.js
@@ -1,9 +1,12 @@
-function hover(id){
+function hover(id) {
     console.log("g")
-    document.getElementById("pref"+id).style.visibility="visible";
+    document.getElementById("pref" + id).style.visibility = "visible";
+    document.getElementById("national").style.opacity = 0.9;
+
 }
 
-function Mout(id){
+function Mout(id) {
     console.log("h")
-    document.getElementById("pref"+id).style.visibility="hidden";
+    document.getElementById("pref" + id).style.visibility = "hidden";
+    document.getElementById("national").style.opacity = 1;
 }
