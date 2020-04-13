ed81b372c02be86c9d8885e30e209380ba2ca814
yujin_sakuma <yujin12623@gmail.com>
2020-04-13 09:37:10 +0900

    Add
    Add material
    New Script

---
+++ map.js
@@ -0,0 +1,9 @@
+function hover(id){
+    console.log("g")
+    document.getElementById("pref"+id).style.visibility="visible";
+}
+
+function Mout(id){
+    console.log("h")
+    document.getElementById("pref"+id).style.visibility="hidden";
+}
