dbbb26e18f859b38f054eb247e4fcc3297244a68
yujin_sakuma <yujin12623@gmail.com>
2020-04-14 22:36:44 +0900

    Up

--- map.js
+++ map.js
@@ -1,12 +1,52 @@
-function hover(id) {
-    console.log("g")
+function Pre_hover(id) {
     document.getElementById("pref" + id).style.visibility = "visible";
     document.getElementById("national").style.opacity = 0.9;
 
 }
 
-function Mout(id) {
-    console.log("h")
+function Pre_hover_out(id) {
     document.getElementById("pref" + id).style.visibility = "hidden";
     document.getElementById("national").style.opacity = 1;
+}
+var Dataset = [];
+
+function regi_hover(id) {
+
+    Dataset = [];
+    switch (id) {
+        case 1:
+            Dataset = [1];
+            break;
+        case 2:
+            Dataset = [2, 3, 4, 5, 6, 7];
+            break;
+        case 3:
+            Dataset = [8, 9, 10, 11, 12, 13, 14];
+            break;
+        case 4:
+            Dataset = [15, 16, 17, 18, 19, 20, 21, 22, 23];
+            break;
+        case 5:
+            Dataset = [24, 25, 26, 27, 28, 29, 30];
+            break;
+        case 6:
+            Dataset = [31, 32, 33, 34, 35];
+            break;
+        case 7:
+            Dataset = [36, 37, 38, 39];
+            break;
+        case 8:
+            Dataset = [40, 41, 42, 43, 44, 45, 46, 47];
+            break;
+    }
+
+    for (var i = 0; i < Dataset.length; i++) {
+        document.getElementById("pref" + Dataset[i]).style.visibility = "visible";
+    }
+}
+
+function regi_hover_out(id) {
+    for (var i = 0; i < Dataset.length; i++) {
+        document.getElementById("pref" + Dataset[i]).style.visibility = "hidden";
+    }
 }
